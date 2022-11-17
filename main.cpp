50bb5bdf4a027daeb53f55d4e629597aee93a7af
Luca Antonelli <116542318+LucaAnto03@users.noreply.github.com>
2022-11-17 22:38:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
+   float a=0, b=0, area=0;
+    int opzione=0;
+    cin >> a >> b >> opzione ;
+    switch (opzione) {
+        case 0:
+            area = a*b*0.5;
+            cout << area ;
+            break;
+        case 1:
+            area = a*a;
+            cout << area ;
+            break;
+        case 2:
+            area = a*b;
+            cout << area ;
+            break;
+    }
    return 0;
 }
